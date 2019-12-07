a368251a633a353f653c3d4ac59c27db9f03c368
AndrewHuntington <andrew.huntington@gmail.com>
2019-12-07 16:47:49 +0900

    add number display manipulation (only one btn)

--- script/script.js
+++ script/script.js
@@ -17,3 +17,16 @@ function multiply(a, b) {
 function operate(func, a, b) {
   return func(a, b);
 }
+
+const numDisplay = document.getElementById('display-numbers');
+
+const btn = document.querySelector('#one');
+btn.addEventListener('click', (e) => {
+  console.log(e.explicitOriginalTarget.innerText);
+  numDisplay.innerText = e.explicitOriginalTarget.innerText;
+});
+
+const btn2 = document.querySelector('#two');
+btn2.addEventListener('click', (e) => {
+  console.log(e.explicitOriginalTarget.innerText);
+});
